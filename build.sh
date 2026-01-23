d4b675eb5f84f51fd250b51a61a99f7da6061c4f
Pawan <pawan.pk980@gmail.com>
2026-01-23 10:06:23 +0530

    Simplify build.sh and add migrations to startCommand as safety check

--- build.sh
+++ build.sh
@@ -17,16 +17,6 @@ python manage.py collectstatic --no-input
 echo "Running migrations..."
 python manage.py migrate --no-input --verbosity 3
 
-# Verify migrations ran by checking if auth_user table exists
-python manage.py dbshell <<EOF
-\dt auth_user
-EOF
-
-if [ $? -ne 0 ]; then
-    echo "ERROR: Migrations failed or table not created!"
-    exit 1
-fi
-
 echo "Creating default superuser..."
 python manage.py create_default_superuser
 
