f58c3bc0a57ea0be8b458349f88800c346977d11
Pawan <pawan.pk980@gmail.com>
2026-01-27 20:07:02 +0530

    Add redirect for /favicon.ico to fix root path 404 errors

--- config/urls.py
+++ config/urls.py
@@ -6,9 +6,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon/favicon.ico', permanent=True)),
     path('', include('apps.core.urls')),
     path('projects/', include('apps.projects.urls')),
     path('blog/', include('apps.blog.urls')),
